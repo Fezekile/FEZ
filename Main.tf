1b322e7c869b22752dbc895953388fd725d2fa75
Fezekile <fezekile101@gmail.com>
2020-02-06 15:14:11 +0200

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -2,3 +2,7 @@ provider "github" {
 	token = "${var.github_token}"
 	organization = "${var.github_organization}"
 } 
+resource "github_repository" "Mom-repo" {
+  name        = "Mom-repo"
+  description = "My new repository for use with Terraform"
+}
