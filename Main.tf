f512c29c61a362166b12906c88297326e98e6feb
Fezekile <fezekile101@gmail.com>
2020-02-06 15:57:26 +0200

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -2,7 +2,7 @@ provider "github" {
 	token = "ad562812b93f4b60da65ce2751544d418727018c" 
 	organization = "Momentum"
 } 
-resource "github_repository" "Fez" {
-	name = "Fez"
+resource "github_repository" "FEZ" {
+	name = "FEZ"
 	description = "My awesome codebase"
 }
