e38236445a502ac32b4bf6b22a312e3fe10e654a
Fezekile <fezekile101@gmail.com>
2020-02-06 15:22:54 +0200

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -2,7 +2,14 @@ provider "github" {
 	token = "6d88445282e24f63816eafe43f11cd2600af4b35"
 	organization = "Momentum"
 } 
-resource "github_repository" "Mom-repo" {
-  name        = "Mom-repo"
-  description = "My new repository for use with Terraform"
+resource "github_repository" "example" {
+  name        = "example"
+  description = "My awesome codebase"
+
+  private = true
+
+  template {
+    owner = "github"
+    repo = "terraform-module-template"
+  }
 }
