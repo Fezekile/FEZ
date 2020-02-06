87e0385648d5321c074fa1dd5f7247166cd93b54
Fezekile <fezekile101@gmail.com>
2020-02-06 15:03:26 +0200

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -2,3 +2,7 @@ provider "github" {
 	token = "a2bcl5..." 
 	organization = "terraform-example"
 } 
+resource "github_repository" "example-repo" {
+  name        = "example-repo"
+  description = "My new repository for use with Terraform"
+}
